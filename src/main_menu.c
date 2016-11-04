ac1c490b1c78ffacd09d1193a5dd11fc59a2e705
Francois Berder <francois.berder@imgtec.com>
2016-11-04 10:17:08 +0000

    main_menu: exit program if exit button is pressed

--- src/main_menu.c
+++ src/main_menu.c
@@ -1,12 +1,22 @@
+#include <stddef.h>
 #include <letmecreate/letmecreate.h>
 #include "gui.h"
 #include "main_menu.h"
+#include "state.h"
+
 
 static int bid = -1;
+
+static void main_menu_gui_handler(int id)
+{
+    if (id == bid)
+        state_machine_exit();
+}
 
 void main_menu_init(void)
 {
     bid = gui_add_button(100,100,50,30,"Exit");
+    gui_set_user_callback(main_menu_gui_handler);
 }
 
 void main_menu_refresh_screen(void)
@@ -27,4 +37,6 @@ void main_menu_release(void)
 {
     if (bid >= 0)
         gui_remove_button(bid);
+
+    gui_set_user_callback(NULL);
 }
