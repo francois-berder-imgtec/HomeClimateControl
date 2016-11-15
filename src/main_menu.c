b75318d942553d565bae35a067f2b86998ef14f1
Francois Berder <francois.berder@imgtec.com>
2016-11-15 13:22:25 +0000

    main_menu: draw creator logo
    Signed-off-by: Francois Berder <francois.berder@imgtec.com>

--- src/main_menu.c
+++ src/main_menu.c
@@ -1,11 +1,14 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <letmecreate/letmecreate.h>
 #include "gui.h"
 #include "main_menu.h"
 #include "state.h"
 
+#define CREATOR_IMAGE_WIDTH             (100)
+#define CREATOR_IMAGE_HEIGHT            (34)
 
 static char* month_str[] = {
     "Jan",
@@ -57,8 +60,63 @@ static void draw_time(void)
                    str);
 }
 
+static int load_image_from_file(char *filepath, char **buffer, uint32_t *buffer_length)
+{
+    FILE *file = fopen(filepath, "rb");
+    if (file == NULL)
+        return -1;
+
+    fseek(file, 0, SEEK_END);
+    *buffer_length = ftell(file);
+    rewind(file);
+
+    *buffer = malloc(*buffer_length);
+    if (buffer == NULL) {
+        fclose(file);
+        return -1;
+    }
+
+    if (fread(*buffer, 1, *buffer_length, file) != *buffer_length) {
+        fclose(file);
+        return -1;
+    }
+    fclose(file);
+
+    return 0;
+}
+
+static void load_creator_logo(void)
+{
+    char *buffer = NULL;
+    uint32_t buffer_length = 0;
+
+    if (load_image_from_file("data/creator_logo.jpg", &buffer, &buffer_length) < 0) {
+        fprintf(stderr, "Failed to load creator logo.\n");
+        return;
+    }
+
+    eve_click_load_image(0,
+                         FT800_OPT_RGB565,
+                         buffer,
+                         buffer_length);
+
+    free(buffer);
+}
+
+static void draw_logo(void)
+{
+    eve_click_draw(FT800_BITMAP_SOURCE, 0);
+    eve_click_draw(FT800_BITMAP_LAYOUT, FT800_RGB565, CREATOR_IMAGE_WIDTH*2, CREATOR_IMAGE_HEIGHT);
+    eve_click_draw(FT800_BITMAP_SIZE, FT800_NEAREST, FT800_BORDER, FT800_BORDER, CREATOR_IMAGE_WIDTH, CREATOR_IMAGE_HEIGHT);
+    eve_click_draw(FT800_BEGIN, FT800_BITMAPS);
+    eve_click_draw(FT800_VERTEX2II, 0, 0, 0, 0);
+    eve_click_draw(FT800_END);
+}
+
 void main_menu_init(void)
 {
+    load_creator_logo();
+
     bid = gui_add_button(220,235,50,30,"Exit");
     gui_set_user_callback(main_menu_gui_handler);
 }
@@ -82,6 +140,7 @@ void main_menu_refresh_screen(void)
                    str);
 
     draw_time();
+    draw_logo();
 
     gui_draw();
     eve_click_display();
